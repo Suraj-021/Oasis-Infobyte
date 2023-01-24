3ae01e2c6e950d5106030d5140193851db53e490
Suraj <Walkesuraj722@gmail.com>
2023-01-24 20:39:50 +0530

    Initial commit

---
+++ Task1- Calculator/script.js
@@ -0,0 +1,25 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'Clear'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    
+    // else if(e.target.innerHTML == 'Del'){
+    //   string = ""
+    //   document.querySelector('input').value.slice(0,-1);
+    // }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
